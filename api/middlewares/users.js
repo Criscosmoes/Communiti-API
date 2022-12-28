72a60827c9aad302834413b737eac946535ecda2
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-12-28 15:20:38 -0800

    added ability to add users and get all users

---
+++ api/middlewares/users.js
@@ -0,0 +1,22 @@
+const checkPayload = async (req, res, next) => {
+    try {
+      const { username, oauth_id } = req.body;
+  
+      if (!username || !oauth_id) {
+        return res
+          .status(400)
+          .send("Please provide a username and oauth id.");
+      }
+  
+      req.payload = req.body;
+  
+      next();
+    } catch (error) {
+      res.status(500).send({ ERROR: error.message, DETAIL: error.detail });
+    }
+  };
+  
+  module.exports = {
+    checkPayload,
+  };
+  
