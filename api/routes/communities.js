f499aba7537b13f2a6553c78a818f73a24ea34b9
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-12-31 14:33:17 -0800

    added routes and queries to search input component

--- api/routes/communities.js
+++ api/routes/communities.js
@@ -58,6 +58,22 @@ router.get("/popular_communities", async (req, res) => {
     }
 })
 
+router.get("/communities/:term", async (req, res) => {
+
+    try {
+        const { term } = req.params; 
+
+        const communities = await Communities.getCommunitiesByTerm(term); 
+
+        res.status(200).send(communities.rows); 
+    } catch (error) {
+        
+        res.status(500).send({ ERROR: error.message, DETAIL: error.detail });
+
+    }
+
+})
+
 router.post("/communities", [mpUpload], async (req, res) => {
 
     try {
